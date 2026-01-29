b2b5e4f1115cf7a363805c5ea9263a26c666f51e
*Rahul <rahulravi7736@gmail.com>
2026-01-29 11:18:59 +0530

    Fix duplicate URL includes and root routing

--- scholarship_project/urls.py
+++ scholarship_project/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('exam.urls')),
-    path('synergyfiesta/', include('exam.urls')),
 ]
